f82592bdbcb2f42b2e85b3d70206cf2849d867a1
aimtrettel <aimeetrettle@MXRadmins-MacBook-Pro.local>
2021-03-03 11:06:57 -0500

    Add error logs

--- plugins/omar-stager-core-plugin/grails-app/services/omar/stager/core/IngestService.groovy
+++ plugins/omar-stager-core-plugin/grails-app/services/omar/stager/core/IngestService.groovy
@@ -68,7 +68,16 @@ class IngestService implements ApplicationContextAware
 				log.debug( "Creating default repository /" )
 			}
 
+			if( !repository )
+			{
+				log.error ("Could not create repository")
+			}
+
 			return repository
+		}
+		else
+		{
+			log.error("IngestService: Does not contain the proper separatorChar")
 		}
 	}
 
