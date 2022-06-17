293a3f0e7187703813643593f42459f9fa5e3881
sbortman <scott.bortman@maxar.com>
2022-06-17 08:08:52 -0400

    hackaton-1

--- plugins/omar-stager-core-plugin/src/main/groovy/omar/stager/core/plugin/OmarStagerCorePluginGrailsPlugin.groovy
+++ plugins/omar-stager-core-plugin/src/main/groovy/omar/stager/core/plugin/OmarStagerCorePluginGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import grails.plugins.*
 class OmarStagerCorePluginGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "4.0.0 > *"
+    def grailsVersion = "5.1.7 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
